bc2ea84d9e2d6b953d8f262e46e9f050735b89dd
bevanvarghese <bevanvar@connect.hku.hk>
2020-09-26 15:55:38 +0530

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 import codeitsuisse.routes.saladspree
 import codeitsuisse.routes.magicalfruitbasket
 import codeitsuisse.routes.revisitgeometry
