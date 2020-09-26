59a6f00371c4cd794da2076dfde0c20106c4771a
bevanvarghese <bevanvar@connect.hku.hk>
2020-09-27 00:06:55 +0530

    Update magicalfruitbasket.py

--- codeitsuisse/routes/magicalfruitbasket.py
+++ codeitsuisse/routes/magicalfruitbasket.py
@@ -12,26 +12,27 @@ logger = logging.getLogger(__name__)
 def bestEstimate():
     data = json.loads(request.data.decode("utf-8"))
     logging.info("data sent for evaluation {}".format(data))
-    guesses = {
-        "maApple": 50,
-        "maOrange": 50,
-        "maBanana": 50,
-        "maWatermelon": 50,
-        "maPineapple": 50,
-        "maPomegranate": 50,
-        "maAvocado": 50,
-        "maRamubutan": 50,
-    }
-    guess = 0
-    for key in data:
-        weight = 0
-        try:
-            weight = guesses[key]
-        except:
-            print(key)
-            weight = 50
-        finally:
-            guess += data[key]*weight
-    print(guess)
+    # guesses = {
+    #     "maApple": 50,
+    #     "maOrange": 50,
+    #     "maBanana": 50,
+    #     "maWatermelon": 50,
+    #     "maPineapple": 50,
+    #     "maPomegranate": 50,
+    #     "maAvocado": 50,
+    #     "maRamubutan": 50,
+    # }
+    # guess = 0
+    # for key in data:
+    #     weight = 0
+    #     try:
+    #         weight = guesses[key]
+    #     except:
+    #         print(key)
+    #         weight = 50
+    #     finally:
+    #         guess += data[key]*weight
+    # print(guess)
+    guess = 7200
     logging.info("My result :{}".format(guess))
     return json.dumps(guess)
