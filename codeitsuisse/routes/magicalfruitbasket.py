6a100c3dfb5043893f5db62e3a827199a6ad5df0
bevanvarghese <bevanvar@connect.hku.hk>
2020-09-26 10:40:22 +0530

    guess

--- codeitsuisse/routes/magicalfruitbasket.py
+++ codeitsuisse/routes/magicalfruitbasket.py
@@ -11,13 +11,9 @@ logger = logging.getLogger(__name__)
 @app.route('/fruitbasket', methods=['POST'])
 def bestEstimate():
     data = request.get_json()
-    numApples = data.get("maApple")
-    numWatermelons = data.get("maWatermelon")
-    numBananas = data.get("maBanana")
-
-    applePrice = 10
-    watermelonPrice = 20
-    bananaPrice = 30
-    output = applePrice*numApples + watermelonPrice * \
-        numWatermelons + bananaPrice*numBananas
-    return output
+    units = list(data.values())
+    guesses = [10, 20, 30]
+    output = 0
+    for i in range(len(units)):
+        output += units[i]*guesses[i]
+    return str(output)
